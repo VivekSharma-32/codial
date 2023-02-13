5ff3535426fc91c33bd20d159dadd2004a82909c
VivekSharma-32 <vivek.sharma1275@gmail.com>
2023-02-13 23:23:54 +0530

    view engine setup

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ const PORT = 8000;
 // use express router
 app.use("/", require("./routes/index"));
 
+// setup the view engine
+app.set("view engine", "ejs");
+app.set("views", "./views");
+
 app.listen(PORT, (err) => {
   if (err) {
     console.log(`Error: ${err}`);
